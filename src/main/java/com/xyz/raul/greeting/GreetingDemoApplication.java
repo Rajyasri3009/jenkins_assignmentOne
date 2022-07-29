abce3e4a5f2eacecf301e4a605cdc038e0f23e89
Rahul Jacob <e.rahuljacob@gmail.com>
2022-07-30 01:54:21 +0530

    greeting code changes

---
+++ src/main/java/com/xyz/raul/greeting/GreetingDemoApplication.java
@@ -0,0 +1,15 @@
+package com.xyz.raul.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingDemoApplication {
+/*
+ * Program Execution begins here...
+ */
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingDemoApplication.class, args);
+	}
+
+}
